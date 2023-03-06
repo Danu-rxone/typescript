279152cc14b05ecd4a39199477d61222d9e6eea0
Danu Itsnan Habibi <danuitsnanhabibi13@gmail.com>
2023-03-06 20:53:25 +0700

    ts type & array and tuples

--- src/2-ts-types.ts
+++ src/2-ts-types.ts
@@ -1,5 +1,90 @@
-// type inference(Implicit)
-let ti = 'hello';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-console */
+/* eslint-disable prefer-const */
 
-// type annotation(Explicit)
-let ta: string = 'hello ts';
+let a; // undefined
+console.log(typeof a);
+let b: boolean;
+b = true; // boolean
+b = false; // boolean
+console.log(typeof b);
+let c: number;
+c = 1; // number
+c = 1.1; // number
+console.log(typeof c);
+let d: string;
+d = 'hello'; // string
+d = '1'; // string
+console.log(typeof d);
+let e: bigint;
+e = 100n; // big int (starting ES2020)
+e = 10000n;
+console.log(typeof e);
+let f: symbol;
+f = Symbol('Sym'); // symbol  (starting ES2015)
+f = Symbol('Sym2');
+console.log(typeof f);
+let g: () => void;
+g = function () {
+    // function
+    return null;
+};
+g = function (): number {
+    // function
+    return 5;
+};
+console.log(typeof g);
+let h: null | undefined;
+h = null; // null ( special primitive )
+h = undefined; // undefined ( special primitive )
+console.log(typeof h);
+let i: Record<any, any>;
+i = {}; // Object Literal
+i = { name: 'John' };
+console.log(typeof i);
+let j: any[];
+j = []; // array
+j = [1, 2, 3];
+console.log(typeof j);
+
+class Product {
+    // class
+    //...
+}
+console.log(typeof Product);
+let k = new Product();
+k = new Product();
+console.log(typeof k);
+
+let z: any;
+z = 1; // any
+z = 'hello';
+z = true;
+z = {};
+z = [];
+console.log(typeof z);
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+console.log(add(1, 2));
+
+function ashiap(a: number, b: number): void {
+    return console.log(a + b);
+}
+console.log(ashiap(1, 2));
+
+// union
+let union: string | number;
+union = 'hello';
+union = 1;
+// union = true; // error
+console.log(union);
+
+// type aliases
+type StringOrNumber = string | number;
+let alias: StringOrNumber;
+alias = 'hello';
+alias = 1;
+// alias = true; // error
+console.log(alias);
--- src/3-array-tuple.ts
+++ src/3-array-tuple.ts
@@ -17,20 +17,50 @@ export {}; // quick fix for global variable
  * [1,2,3,4]
  */
 
+// type inference
+const arr1 = [1, 2, 3, 4];
+// square bracket
+let arr2: number[];
+arr2 = [1, 2, 3, 4];
+// generic
+let arr3: Array<number>;
+arr3 = [1, 2, 3, 4];
+
 /**
  * 2. Array of String
  * ['a', 'b', 'c']
  */
+// type inference
+const arr4 = ['a', 'b', 'c'];
+// square bracket
+let arr5: string[];
+arr5 = ['q', 'r', 't'];
+//generic
+let arr6: Array<string>;
+arr6 = ['q', 'r', 't'];
 
 /**
  * 3. Array with Union
  * ["hello", 1, 2, 3 ]
  */
 
+//type inference
+const arr7 = ['hello', 1, 2, 3];
+// square bracket
+let arr8: (string | number)[];
+arr8 = [1, 2, 3, 'halo'];
+//generic
+let arr9: Array<number | string>;
+arr9 = ['hello', 1, 2, 3];
+
 /**
  * 4. Tuple
  * ['cordinat', 2, 4, 7]
  */
+
+// tidak bisa palkai inference
+let list4: [string, number, boolean];
+list4 = ['halo', 4, false];
 
 /**
  * 5. Array of Object
@@ -40,6 +70,20 @@ export {}; // quick fix for global variable
  * ]
  */
 
+// inline interface
+let list5_1: { color: string; index: number }[];
+list5_1 = [
+    { color: 'blue', index: 1 },
+    { color: 'red', index: 2 },
+];
+
+// generic
+let list5_2: Array<{ color: string; index: number }>;
+list5_2 = [
+    { color: 'blue', index: 1 },
+    { color: 'red', index: 2 },
+];
+
 /**
  * 6. Array multi dimension [[]]
  * matrix = [
@@ -48,7 +92,23 @@ export {}; // quick fix for global variable
  * ];
  */
 
+// square bracket
+let matrix: number[][];
+matrix = [
+    [1, 2],
+    [3, 4],
+];
+
+// generic
+let matrix2: Array<Array<number | string>>;
+matrix2 = [
+    [1, 2, 'halo'],
+    [3, 4],
+];
+
 /**
  * 7. Destructuring
  * let [a, b, c, d]: [number, number, number, string] = [1, 2, 3, 'four'];
  */
+
+let [a, b, c, d]: [number, number, number, string] = [1, 2, 3, 'four'];
